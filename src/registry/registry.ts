9bbd1c825d360039dae48b599797e1b08cfee060
Ryan Jabbour <148877520+ryanjabbour@users.noreply.github.com>
2025-03-04 11:06:15 +0100

    Update registry.ts

--- src/registry/registry.ts
+++ src/registry/registry.ts
@@ -18,8 +18,28 @@ export async function launchRegistry() {
   _registry.use(express.json());
   _registry.use(bodyParser.json());
 
-  // TODO implement the status route
-  // _registry.get("/status", (req, res) => {});
+  const nodes: Node[] = [];
+
+  _registry.get("/status", (req, res) => {
+    res.send("live");
+  });
+  
+  _registry.post("/registerNode", (req, res) => {
+    const { nodeId, pubKey } = req.body as RegisterNodeBody;
+    
+    const existingNode = nodes.find(node => node.nodeId === nodeId);
+    if (existingNode) {
+      existingNode.pubKey = pubKey;
+    } else {
+      nodes.push({ nodeId, pubKey });
+    }
+    
+    res.send("success");
+  });
+  
+  _registry.get("/getNodeRegistry", (req, res) => {
+    res.json({ nodes });
+  });
 
   const server = _registry.listen(REGISTRY_PORT, () => {
     console.log(`registry is listening on port ${REGISTRY_PORT}`);
